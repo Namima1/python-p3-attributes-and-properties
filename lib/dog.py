d1969b4981695c51e00644377251bc10fcea8e5e
Namima Islam <namimaislam308@gmail.com>
2024-10-30 20:23:34 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Default String", breed="Beagle"):
+        # if (type(name) is str) and (1 <= len(name) <= 25):
+        self.name = name
+        self.breed = breed
+        # else: 
+        #     print("Name must be string between 1 and 25 characters.")
+        
+    def get_name(self):
+        return self._name 
+        
+    def set_name(self, name):
+        if (type(name) is str) and (1 <= len(name) <= 25):
+            self._name = name
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+    name = property(fset=set_name, fget=get_name) 
+    
+    def get_breed(self):
+        return self._breed 
+        
+    def set_breed(self, breed):
+        approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+        if (breed in approved_breeds):
+            self._breed = breed
+        else: 
+            print("Breed must be in list of approved breeds.")
+    breed = property(fset=set_breed, fget=get_breed) 
+    
+    
+    
+#name is property
+#_name is correspondent attribute
+#use properties to protect attribute 
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Default Argument", job="Default Argument"):
+        self.name = name
+        self.job = job
+        
+    def get_name(self):
+        return self._name 
+    
+    def set_name(self, name):
+        if (type(name) is str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+    name = property(fset=set_name, fget=get_name)
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]  
+        if (job in approved_jobs):
+            self._job = job
+        else: 
+            print("Job must be in list of approved jobs.")
+    job = property(fset=set_job, fget=get_job)   
